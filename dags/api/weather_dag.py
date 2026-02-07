2688da9719bd8544a118eabc7b3544b63a17fdfa
marvisyeh <bodyyeh@gmail.com>
2026-02-07 17:04:56 +0800

    feat: add airflow trigger dbt

--- dags/api/weather_dag.py
+++ dags/api/weather_dag.py
@@ -3,9 +3,12 @@ from datetime import timedelta
 from airflow import DAG
 from airflow.providers.standard.operators.python import PythonOperator
 from airflow_clickhouse_plugin.operators.clickhouse import ClickHouseOperator
-from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+from airflow.operators.bash import BashOperator
 
-from raw.api.weather import GetWeatherDataAPI
+# from cosmos import DbtDag, ProjectConfig, ProfileConfig
+# from cosmos.profiles import ClickhouseUserPasswordProfileMapping
+
+from api.weather import GetWeatherDataAPI
 
 
 def get_raw_weather_data(**context):
@@ -42,46 +45,30 @@ raw_weather_data = PythonOperator(
 
 ## TODO 檢查分層策略以及轉成view效能和刪除機制
 ## ex: 如果以月季年為單位，需要考慮刪除機制，當跨月季年時，需要刪除該月季年的資料
-create_table = ClickHouseOperator(
-    task_id='create_table',
+create_view_from_parquet = ClickHouseOperator(
+    task_id='create_view_from_parquet',
     sql="""
-    CREATE TABLE IF NOT EXISTS raw.raw_api__weathers (
-      elementName Nullable(String),
-      locationName String,
-      startTime DateTime,
-      endTime DateTime,
-      parameterName Nullable(String),
-      parameterValue Nullable(String),
-      parameterUnit Nullable(String),
-      loaded_at DateTime DEFAULT now()
-    ) ENGINE = ReplacingMergeTree(loaded_at)
-    ORDER BY (locationName, startTime, elementName)
-    PRIMARY KEY (locationName, startTime)
-    """,
-    clickhouse_conn_id='clickhouse_raw',
-    dag=dag
-)
-
-insert_data_from_parquet = ClickHouseOperator(
-    task_id='insert_data_from_parquet',
-    sql="""
-    INSERT INTO raw.raw_api__weathers (elementName, locationName, startTime, endTime, parameterName, parameterValue, parameterUnit)
-    SELECT 
+    CREATE VIEW raw_api__weathers AS
+    SELECT
       elementName,
       locationName,
-      toDateTime(startTime) as startTime,
-      toDateTime(endTime) as endTime,
+      startTime,
+      endTime,
       parameterName,
       parameterValue,
       parameterUnit
     FROM file('{{ ti.xcom_pull(task_ids='get_raw_weather_data', key='file_path') }}', 'Parquet')
-    WHERE locationName IS NOT NULL
     """,
     clickhouse_conn_id='clickhouse_raw',
     dag=dag
 )
 
-## TODO: trigger dbt transform task
+## TODO 可以研究其他更好的執行方式
+trigger_transform_data = BashOperator(
+    task_id='trigger_transform_data',
+    bash_command='cd /opt/airflow/dbt && DBT_PROFILES_DIR=/opt/airflow/dbt/.dbt dbt run --select stg_api__weathers+',
+    dag=dag
+)
 
 
-raw_weather_data >> create_table >> insert_data_from_parquet
+raw_weather_data >> create_view_from_parquet >> trigger_transform_data
